7124bae4f991bac93abfb14757d741111593948e
Takizawa-Zawao <106427546+Takizawa-Zawao@users.noreply.github.com>
2022-06-07 23:11:19 +0900

    ok

--- js/script_1.js
+++ js/script_1.js
@@ -1,12 +1,9 @@
 function addGuest(){
-    alert("start");
 
     //既存の「お連れ様」数を取得し、次のお連れ様のナンバリングを決定
     var guests = document.getElementsByClassName("guest");
     var next_guest_count = guests.length + 1;    
     
-    alert(next_guest_count);
-
     //既存のdivを差し替える形で、「お連れ様」情報入力エリアを新規追加
     var new_guest = document.createElement("ul");
     new_guest.className = "guest";
@@ -34,9 +31,6 @@ function addGuest(){
     new_guest_phonetic_input.name = "phonetic_" + parseInt(next_guest_count);
     new_guest_phonetic.appendChild(new_guest_phonetic_input);
     new_guest.appendChild(new_guest_phonetic);
-
-    alert("ok_3");
-    alert(new_guest);
 
     var new_guest_area = document.getElementById("add_guest");
     var next_guest_area = new_guest_area.cloneNode();
