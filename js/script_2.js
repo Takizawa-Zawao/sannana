5e9d2e4049e9f13933664848af06623e1ffaee5d
Takizawa-Zawao <106427546+Takizawa-Zawao@users.noreply.github.com>
2022-06-08 00:16:15 +0900

    ok

--- js/script_2.js
+++ js/script_2.js
@@ -1,4 +1,5 @@
 function initialize(){
+    alert("initiaklize.");
     document.getElementById("add_button").onclick = addGuest();
     document.getElementById("remove_button").onclick = removeGuest();
 }
