b2abf55bba2fb94502773a32168278a24f19a80b
Takizawa-Zawao <106427546+Takizawa-Zawao@users.noreply.github.com>
2022-06-19 20:10:30 +0900

    ok

--- js/script_4.js
+++ js/script_4.js
@@ -1,7 +1,18 @@
 document.querySelectorAll(div.draggable_element).forEach(
     element => {
-        element.onmousedown = function(event){
-            
-        }
+        element.ondragstart = function(event){
+            event.dataTransfer.setData("text/plain", event.target.id);
+        };
+        element.ondragover = function(){
+            event.preventDefault();
+            this.style.borderTop = "3px solid";
+        };
+        element.ondrop = function(){
+            event.preventDefault();
+            var id = event.sataTransfer.getData("text");
+            var element_dropped = document.getElementById(id);
+            this.parentNode.insertBefore(element_dropped, this);
+            this.style.borderTop = "";
+        };
     }
-)
+);
