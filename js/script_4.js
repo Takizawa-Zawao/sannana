8a279a3687b6252689171754fe93979fd7644948
Takizawa-Zawao <106427546+Takizawa-Zawao@users.noreply.github.com>
2022-06-19 20:41:03 +0900

    ok

--- js/script_4.js
+++ js/script_4.js
@@ -1,18 +1,53 @@
-document.querySelectorAll(div.draggable_element).forEach(
-    element => {
-        element.ondragstart = function(event){
-            event.dataTransfer.setData("text/plain", event.target.id);
-        };
-        element.ondragover = function(){
-            event.preventDefault();
-            this.style.borderTop = "3px solid";
-        };
-        element.ondrop = function(){
-            event.preventDefault();
-            var id = event.sataTransfer.getData("text");
-            var element_dropped = document.getElementById(id);
-            this.parentNode.insertBefore(element_dropped, this);
-            this.style.borderTop = "";
-        };
-    }
-);
+function mouseFuction() {
+    document.querySelectorAll(div.draggable_element).forEach(
+        element => {
+            element.ondragstart = function (event) {
+                event.dataTransfer.setData("text/plain", event.target.id);
+            };
+            element.ondragover = function () {
+                event.preventDefault();
+                this.style.borderTop = "3px solid";
+            };
+            element.ondrop = function () {
+                event.preventDefault();
+                var id = event.sataTransfer.getData("text");
+                var element_dropped = document.getElementById(id);
+                this.parentNode.insertBefore(element_dropped, this);
+                this.style.borderTop = "";
+            };
+
+        }
+    );
+}
+function touchFunction() {
+    document.querySelectorAll(div.draggable_element).forEach(
+        element => {
+            element.addEventListener("touchstart", startFunction); 
+            element.addEventListener("touchmove", mediumFunction);
+            element.addEventListener("touchend", endFunction);
+        }
+    );
+}
+
+function startFunction(){
+    event.dataTransfer.setData("text/plain", event.target.id);
+}
+
+function mediumFunction(){
+    event.preventDefault();
+    this.style.borderTop = "3px solid";
+}
+
+function endFunction(){
+    event.preventDefault();
+    var id = event.sataTransfer.getData("text");
+    var element_dropped = document.getElementById(id);
+    this.parentNode.insertBefore(element_dropped, this);
+    this.style.borderTop = "";
+}
+
+if("ontouchend" in document){
+    touchFunction;
+}else{
+    mouseFuction;
+}
