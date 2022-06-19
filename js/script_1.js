da6eef872f542109f32111af8a645e56b35bd38c
Takizawa-Zawao <106427546+Takizawa-Zawao@users.noreply.github.com>
2022-06-19 16:17:40 +0900

    ok

--- js/script_1.js
+++ js/script_1.js
@@ -105,7 +105,7 @@ function addGuest(){
     new_guest.appendChild(new_guest_isMagician);
     */
     var new_guest_isMagician = document.createElement("li");
-    var new_guest_isMagician_text = "ステージマジックの経験はありますか。<br><input type=\"radio\" name=\"isMagician\" value=\"1\">ある<input type=\"radio\" name=\"isMagician\" value=\"0\">ない";
+    var new_guest_isMagician_text = "<p>ステージマジックの経験はありますか。<br><input type=\"radio\" name=\"isMagician\" value=\"1\">ある<input type=\"radio\" name=\"isMagician\" value=\"0\">ない</p>";
     new_guest_isMagician.innerHTML = new_guest_isMagician_text;
     new_guest.appendChild(new_guest_isMagician);
     
